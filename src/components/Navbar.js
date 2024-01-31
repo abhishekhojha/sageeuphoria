5b5646f3d4f471298461230d104aa56d5473d193
abhishekhsageeuphoria <git config --global user.email abhishekhsageeuphoria@gmail.com>
2024-01-31 16:28:40 +0530

    web

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return<>
+        <div className="sageEuphoriaNavbar">
+            <div className="sageEuphoriaNavbarup">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+        </div>
+    </>
+}
