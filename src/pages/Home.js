c0795a21977ad2d84d1f01116b35934c8ef42594
abhishekhojha <abhishekhojha45@gmail.com>
2024-03-31 15:12:50 +0530

    29_03_24 abhi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,22 +20,22 @@ export default function Home() {
     <>
       <div>
         <div className="sageheader">
-        <Reveal>
-              <div className="sageheaderconthead">
-                <h1>SAGE EUPHORIA</h1>
-                <p>
-                  Unleashing the Ultimate campus experience A celebration of
-                  youth, talent, and creativity..
-                </p>
-              </div>
-            </Reveal>
+          <Reveal>
+            <div className="sageheaderconthead">
+              <h1>SAGE EUPHORIA</h1>
+              <p>
+                Unleashing the Ultimate campus experience A celebration of
+                youth, talent, and creativity..
+              </p>
+            </div>
+          </Reveal>
           <div className="sageheadervideo">
-            <video  autoPlay={true} loop muted>
+            <video autoPlay={true} loop muted>
               <source src="./gojo.mp4" type="video/mp4" />
             </video>
           </div>
           <div className="sageheadercont container">
-           
+
             <Reveal>
               <div className="sageheadercontBtn">
                 <a href="/#" className="sagebuttons">
@@ -43,7 +43,7 @@ export default function Home() {
                 </a>
                 <div className="scroll">
                   <p>Scroll</p>
-                  <img style={{filter: "invert(1)"}} src="./down.png" alt="" />
+                  <img style={{ filter: "invert(1)" }} src="./down.png" alt="" />
                 </div>
                 <a href="/#" className="sagebuttons">
                   Get your passes
@@ -67,13 +67,13 @@ export default function Home() {
                     celebration to remember? SAGE Euphoria is your opportunity
                     to experience a weekend full of fun, learning, and
                     entertainment! From 27th to 29th March 2023, we invite you
-                    to join us for the ultimate college fest. 
+                    to join us for the ultimate college fest.
                   </p>
                 </div>
               </div>
               <div className="sageeventsaboutusSecond">
                 <p>
-                SAGE Euphoria 2.0 presents an unforgettable celebration of college life! From March 11th to 13th, 2024, immerse yourself in a dynamic blend of entertainment, education, and camaraderie. Experience a diverse range of activities, including technical, cultural, literary , E-sports competitions. Dive into special events like the entrepreneurship conclave, creator's conclave, Shark Tank Sage, and electrifying live concerts. Engage in enriching workshops spanning various fields. SAGE Euphoria offers the perfect platform to forge enduring memories, expand your skill set, connect with peers, and revel in the essence of college life. Don't miss out on this opportunity to embrace the spirit of learning, growth, and celebration!
+                  SAGE Euphoria 2.0 presents an unforgettable celebration of college life! From March 11th to 13th, 2024, immerse yourself in a dynamic blend of entertainment, education, and camaraderie. Experience a diverse range of activities, including technical, cultural, literary , E-sports competitions. Dive into special events like the entrepreneurship conclave, creator's conclave, Shark Tank Sage, and electrifying live concerts. Engage in enriching workshops spanning various fields. SAGE Euphoria offers the perfect platform to forge enduring memories, expand your skill set, connect with peers, and revel in the essence of college life. Don't miss out on this opportunity to embrace the spirit of learning, growth, and celebration!
                 </p>
               </div>
             </>
@@ -107,18 +107,13 @@ export default function Home() {
               <div className="sagepassesconin">
                 <div className="sagepassesconinImage">
                   <img src="./artist/1.jpg" alt="" />
-                  <h3>AKHIL SACHDEVA</h3>
                 </div>
                 <div className="sagepassesconincon">
+                  <h3>AKHIL SACHDEVA</h3>
                   <p>
-                    Sahil Sharma, better known by his stage name Zaeden, is a
-                    talented singer and record producer who is set to perform at
-                    the Sage Euphoria night concert. Hailing from India, Zaeden
-                    has gained a reputation for his unique style of blending
-                    electronic dance music with Indian classical music. He first
-                    rose to fame with his remix of Coldplay's hit song "Magic"
-                    which gained millions of views on YouTube.
+                    Akhil Sachdeva is a multi-talented Indian musician who wears many hats - singer, songwriter, and composer. He rose to fame with his soulful ballad "Humsafar" from the Bollywood film "Badrinath Ki Dulhania." Sachdeva's voice has captivated audiences, and his compositions have become chart-topping hits like "Tera Ban Jaunga" from "Kabir Singh."  Be prepared to be enthralled by his musical magic!
                   </p>
+                  <p>20<sup>th</sup> April At Sage university</p>
                 </div>
               </div>
             </XReveal>
@@ -143,18 +138,13 @@ export default function Home() {
               <div className="sagepassesconin">
                 <div className="sagepassesconinImage">
                   <img src="./artist/2.jpg" alt="" />
-                  <h3>DJ RHEA</h3>
                 </div>
                 <div className="sagepassesconincon">
+                  <h3>DJ RHEA</h3>
                   <p>
-                    Sahil Sharma, better known by his stage name Zaeden, is a
-                    talented singer and record producer who is set to perform at
-                    the Sage Euphoria night concert. Hailing from India, Zaeden
-                    has gained a reputation for his unique style of blending
-                    electronic dance music with Indian classical music. He first
-                    rose to fame with his remix of Coldplay's hit song "Magic"
-                    which gained millions of views on YouTube.
+                    Hailing from the Indian electronic dance scene, DJ Rhea brings the party wherever she goes. A master of setting the mood, she seamlessly blends genres from Bollywood to EDM, keeping the dance floor packed with infectious energy.  Having performed at major events like IPL after-parties and music festivals, DJ Rhea is sure to get your feet moving and create an unforgettable experience.
                   </p>
+                  <p>19<sup>th</sup> April At Sage university</p>
                 </div>
               </div>
             </YReveal>
