8e8a664403445b03e1913e77aa1364a110d096c6
abhishekhojha <abhishekhojha45@gmail.com>
2024-04-03 01:35:39 +0530

    1_04_24 abhi 17

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,17 +4,19 @@ import Reveal from "../anime/Animation";
 import { SlideshowLightbox } from "lightbox.js-react";
 import { gallery } from "../anime/gallery";
 export default function Gallery() {
-  const [imgdata, setimgdata] = useState(gallery.slice(0, 9));
+  const [imgdata, setimgdata] = useState([gallery.slice(0, 9)]);
   const [count, setcount] = useState(1);
-
+  const limit = Math.ceil(gallery.length/9);
   useEffect(() => {
     // window.scrollTo(0, 0);
     console.log(imgdata);
   }, [imgdata, SlideshowLightbox]);
   const loadmore = () => {
-    var skip = count * 9;
-    var val = (count + 1) * 9;
-    setimgdata((prev) => [...prev, ...gallery.slice(skip, val)]);
+    var val = ((count + 1) * 9) - 1;
+    let skip = val - 9 <= 0 ? val - 8 : val - 9;
+    const data = imgdata;
+    data.push([...gallery.slice(skip, val)])
+    setimgdata(data);
     setcount(count + 1);
   };
   return (
@@ -30,18 +32,27 @@ export default function Gallery() {
         </Reveal>
 
         <div className="px-4">
-          <SlideshowLightbox className="container grid grid-cols-2 px-2 md:grid-cols-3 gap-2 mx-auto">
-            {imgdata?.map((img) => (
-              <img
-                key={img.id}
-                className="h-full object-cover w-full rounded"
-                src={img.imageSrc}
-              />
-            ))}
-          </SlideshowLightbox>
-          <button onClick={loadmore}>Load more</button>
+          <>
+            {imgdata?.map((images) => {
+              console.log(images)
+              return (
+                < SlideshowLightbox className="container gallery_grid grid grid-cols-2 px-2 md:grid-cols-3 gap-2 mx-auto" >
+                  {images?.map((img) => (
+                    <img
+                      key={img.id}
+                      className="h-full object-cover w-full rounded"
+                      src={img.imageSrc}
+                      alt=""
+                    />
+                  ))
+                  }
+                </SlideshowLightbox>
+              )
+            })}
+            {limit>count?<button className="sagebuttons mt-2" onClick={loadmore}>Load more...</button>:<></>}
+          </>
         </div>
-      </div>
+      </div >
     </>
   );
 }
