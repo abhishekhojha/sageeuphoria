f42b46c7faf6d292c8646b82fcc7ed9bfabf3324
yash <ymandlekar9826@gmail.com>
2024-04-03 00:07:51 +0530

    gallery update

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -8,14 +8,14 @@ export default function Gallery() {
   const [count, setcount] = useState(1);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    // window.scrollTo(0, 0);
+    console.log(imgdata);
+  }, [imgdata,SlideshowLightbox]);
   const loadmore = () => {
     var skip = count * 9;
     var val = (count + 1) * 9;
-    setimgdata((prev) => [...prev, gallery.slice(skip, val)]);
+    setimgdata((prev) => [...prev, ...gallery.slice(skip, val)]);
     setcount(count + 1);
-    console.log(val);
   };
   return (
     <>
@@ -29,14 +29,17 @@ export default function Gallery() {
           </div>
         </Reveal>
 
-        <div className="px-4 min-h-64">
+        <div className="px-4">
           <SlideshowLightbox className="container grid grid-cols-2 px-2 md:grid-cols-3 gap-2 mx-auto">
-            {imgdata.map((img) => (
+            {imgdata?.map((img) => (
               <img key={img.id} className="w-full rounded" src={img.imageSrc} />
             ))}
           </SlideshowLightbox>
           <button onClick={loadmore}>Load more</button>
         </div>
+            {/* {imgdata?.map((img) => (
+              <img height="100" key={img.id} className="w-full rounded" src={img.imageSrc} />
+            ))} */}
       </div>
     </>
   );
