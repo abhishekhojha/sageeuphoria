26d50dddfd7d135f25eed4f7c965422606308353
yash <ymandlekar9826@gmail.com>
2024-04-10 01:08:42 +0530

    by yash

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,22 +4,47 @@ import Reveal from "../anime/Animation";
 import { SlideshowLightbox } from "lightbox.js-react";
 import { gallery } from "../anime/gallery";
 export default function Gallery() {
+  const [image, setImage] = useState(null);
   const [imgdata, setimgdata] = useState([gallery.slice(0, 9)]);
   const [count, setcount] = useState(1);
-  const limit = Math.ceil(gallery.length/9);
+  const limit = Math.ceil(gallery.length / 9);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   const loadmore = () => {
-    var val = ((count + 1) * 9) - 1;
+    var val = (count + 1) * 9 - 1;
     let skip = val - 9 <= 0 ? val - 8 : val - 9;
     const data = imgdata;
-    data.push([...gallery.slice(skip, val)])
+    data.push([...gallery.slice(skip, val)]);
     setimgdata(data);
     setcount(count + 1);
   };
+  const handleOverlay = (e) => {
+    if (
+      e.target.classList.value === "overlay" ||
+      e.target.classList.value === "close" ||
+      e.target.classList.value === "close-img"
+    ) {
+      setImage(null);
+    }
+  };
+  const handleImage = (e) => {
+    setImage(e);
+  };
+  useEffect(() => {
+    window.addEventListener("click", handleOverlay);
+    return () => {
+      window.removeEventListener("click", handleOverlay);
+    };
+  }, []);
   return (
     <>
+      {image && (
+        <div className="overlay">
+          <div className="close">X</div>
+          <img className="close-img" src={image} alt="" />
+        </div>
+      )}
       <div className="sageEvents container">
         <Reveal>
           <div className="sageEventsHead">
@@ -31,23 +56,31 @@ export default function Gallery() {
           <>
             {imgdata?.map((images) => {
               return (
-                < SlideshowLightbox className="container gallery_grid grid grid-cols-2 px-2 md:grid-cols-3 gap-2 mx-auto" >
+                <div className="container gallery_grid grid grid-cols-2 px-2 md:grid-cols-3 gap-2 mx-auto">
                   {images?.map((img) => (
                     <img
                       key={img.id}
                       className="h-[150px] lg:h-full object-cover w-full rounded"
                       src={img.imageSrc}
+                      onClick={() => handleImage(img.imageSrc)}
                       alt=""
                     />
-                  ))
-                  }
-                </SlideshowLightbox>
-              )
+                  ))}
+                </div>
+              );
             })}
-            {limit>count?<div className="grid items-center justify-center py-6"><button className="sagebuttons mt-2" onClick={loadmore}>Load more...</button></div>:<></>}
+            {limit > count ? (
+              <div className="grid items-center justify-center py-6">
+                <button className="sagebuttons mt-2" onClick={loadmore}>
+                  Load more...
+                </button>
+              </div>
+            ) : (
+              <></>
+            )}
           </>
         </div>
-      </div >
+      </div>
     </>
   );
 }
