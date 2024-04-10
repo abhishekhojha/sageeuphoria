f4c0f9e42035c6d5f71f75e12265b46cdb4e43c7
yash <ymandlekar9826@gmail.com>
2024-04-10 15:13:55 +0530

    04-04 abhi

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,15 +4,15 @@ import Reveal from "../anime/Animation";
 import { gallery } from "../anime/gallery";
 export default function Gallery() {
   const [image, setImage] = useState(null);
-  const [imgdata, setimgdata] = useState([gallery.slice(0, 9)]);
+  const [imgdata, setimgdata] = useState([gallery.slice(0, 10)]);
   const [count, setcount] = useState(1);
-  const limit = Math.ceil(gallery.length / 9);
+  const limit = Math.ceil(gallery.length / 10);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   const loadmore = () => {
-    var val = (count + 1) * 9 - 1;
-    let skip = val - 9 <= 0 ? val - 8 : val - 9;
+    var val = (count + 1) * 10 - 1;
+    let skip = val - 10 <= 0 ? val - 9 : val - 10;
     const data = imgdata;
     data.push([...gallery.slice(skip, val)]);
     setimgdata(data);
@@ -35,6 +35,7 @@ export default function Gallery() {
       window.removeEventListener("click", handleOverlay);
     };
   }, []);
+  let i = 0;
   return (
     <>
       {image && (
@@ -52,17 +53,18 @@ export default function Gallery() {
 
         <div className="px-4">
           <>
-            {imgdata?.map((images) => {
+            {imgdata?.map((images,j) => {
               return (
-                <div className="container gallery_grid grid grid-cols-2 px-2 md:grid-cols-3 gap-2 mx-auto">
-                  {images?.map((img) => (
-                    <img
-                      key={img.id}
-                      className="h-[150px] lg:h-full object-cover w-full rounded"
-                      src={img.imageSrc}
-                      onClick={() => handleImage(img.imageSrc)}
-                      alt=""
-                    />
+                <div key={j} className="container gallery_grid grid px-2 grid-cols-3 gap-2 mx-auto">
+                  {images?.map((img,i) => (
+                    <div key={img.id} className={i==2?"row-start-1	row-end-3 col-start-3":i==3?"row-start-3	row-end-5 col-start-1":""}>
+                      <img
+                        className="h-full object-cover w-full rounded"
+                        src={img.imageSrc}
+                        onClick={() => handleImage(img.imageSrc)}
+                        alt=""
+                      />
+                    </div>
                   ))}
                 </div>
               );
